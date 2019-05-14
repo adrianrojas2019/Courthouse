1db21167682e00c808b5f615b58ac7a321242468
Adrian Rojas <rojasadrian@hotmail.com>
2019-05-14 07:00:40 -0600

    searchmidlandmapsbycountyandyear

--- target/classes/main/java/pageObjects/MidlandMapsPage.java
+++ target/classes/main/java/pageObjects/MidlandMapsPage.java
@@ -9,9 +9,13 @@ import org.openqa.selenium.WebDriver;
 public class MidlandMapsPage extends PageObjects  {
 
     private final By COUNTY_COMBO = By.xpath("//form[@name='midlandForm']//div[@class='filter-input-container col-xs-8']//select");
-    private final By MIDLAND_MAPS_COUNTY = By.xpath("");
+    private final By COUNTY_YEAR_COMBO = By.xpath("//form[@name='midlandForm']//select[@class='form-control ng-pristine ng-untouched ng-scope ng-invalid ng-invalid-required']");
+    private final By APPLY_BUTTON = By.xpath("//aside[@ui-view='filtersMidland']//button[@class='btn btn-primary'][@type='submit']");
+    private final By SPINNER_POPUP = By.xpath("//section[@ng-show='searchFilterMidlandSearchOpen']//article[@di-pdf-viewer='pdfViewerOptions']//div[@class='spinner']");
+    private final By DOCUMENT_NOT_AVAILABLE = By.xpath("//canvas[@width=856][@height=1108]");
 
     String findMidlandMapsCounty = "//div[@class='filter-input-container col-xs-8']//option[text()='%s']";
+    String findMidlandMapsCountyYear = "//option[text()='%s']";
 
     public MidlandMapsPage(WebDriver driver) {
         super(driver);
@@ -23,8 +27,32 @@ public class MidlandMapsPage extends PageObjects  {
         webDriverCommands.waitSomeSeconds(5);
     }
 
+    public void clickOnYearCombo(String contractorCountyYear){
+        webDriverCommands.waitSomeSeconds(5);
+        webDriverCommands.type(COUNTY_YEAR_COMBO,contractorCountyYear);
+        webDriverCommands.waitSomeSeconds(5);
+    }
+    public void clickOnApplyButton(){
+        webDriverCommands.waitSomeSeconds(3);
+        webDriverCommands.click(APPLY_BUTTON);
+        webDriverCommands.waitSomeSeconds(3);
+    }
+
+    public void isProgressBarDone() {
+        webDriverCommands.waitForElementInVisible(SPINNER_POPUP, 260);
+    }
+
+    public boolean isDocumentNotAvailable(){
+        return webDriverCommands.waitForElementPresent(DOCUMENT_NOT_AVAILABLE,3);
+    }
+
     public boolean isMidlandMapCountyDisplayed(String countyMidlandMaps){
-        webDriverCommands.waitSomeSeconds(1);
+        webDriverCommands.waitSomeSeconds(2);
         return webDriverCommands.waitForElementPresent(By.xpath(String.format(findMidlandMapsCounty, countyMidlandMaps)));
+    }
+
+    public boolean isMidlandMapCountyYearDisplayed(String countyYearMidlandMaps){
+        webDriverCommands.waitSomeSeconds(2);
+        return webDriverCommands.waitForElementPresent(By.xpath(String.format(findMidlandMapsCountyYear, countyYearMidlandMaps)));
     }
 }
--- target/classes/main/java/tests/contractorTests/contractorMidlandMapsSearch.java
+++ target/classes/main/java/tests/contractorTests/contractorMidlandMapsSearch.java
@@ -15,77 +15,35 @@ import org.testng.annotations.Test;
  * Created by Adrian on 12/10/2018.
  */
 public class contractorMidlandMapsSearch extends SeleniumInitializer {
-    @Parameters({"environment","superUserName","superUserPassword","contractorUserName","contractorPassword","myCompanyName","countyMidlandMaps"})
+    @Parameters({"environment","contractorUserName","contractorPassword","searchMidlandMapsCountyName","searchMidlandMapsCountyYear"})
     @Test(groups = {"CH_Contractor_Midland_Maps_Search", "Regression","Midland_Maps_Feature"})
 
-    public void contractorMidlandMapsSearch(String environment,String superUserName, String superUserPassword, String contractorUserName, String contractorPassword,String myCompanyName, String countyMidlandMaps) throws InterruptedException {
+    public void contractorMidlandMapsSearch(String environment,String contractorUserName, String contractorPassword,String searchMidlandMapsCountyName, String searchMidlandMapsCountyYear) throws InterruptedException {
 
 
         //Already logged in as DI Admin
         loginTest loginIntoCHMainPageTest = new loginTest();
         //Create page object
-        ExplorerPage newExplorerPage = loginIntoCHMainPageTest.loginSuccessfully(superUserName, superUserPassword, getDriverInstance());
-
-        //Go to User Administration Page
-        UserAdministrationPage newUserAdminPage = newExplorerPage.clickOnUserAdministrationPage();
-        //Validate if the user menu has been selected/displayed
-        Assert.assertTrue(newUserAdminPage.isMenuUserDisplayed(10), "Cannot display the User Menu");
-        //Click on User Administration item
-        newUserAdminPage.clickOnUserAdministrationItem();
-        //Validate that User Grid has been displayed
-        Assert.assertTrue(newUserAdminPage.isUserAdminGridDisplayed(), "Cannot access the User Administration Grid.");
-
-        //Find contractor user
-        newUserAdminPage.clickOnSearchFieldAsSuperUser(contractorUserName);
-        //Validate that user has been found
-        Assert.assertEquals(1, NumberUtils.toInt(newUserAdminPage.isUserFound()), "Cannot find the Contract User.");
-        //Click on Contractor User Name
-        newUserAdminPage.clickOnContractorName(myCompanyName);
-        //Add on Unassigned counties search text box: CountyMidland Map
-        newUserAdminPage.addMidlandMapsCounty(countyMidlandMaps);
-        newUserAdminPage.clickOnCountyMidlandMaps(countyMidlandMaps);
-        Assert.assertTrue(newUserAdminPage.isUnassignedCountyDisplayed(countyMidlandMaps), "Cannot find County: " + countyMidlandMaps);
-
-        //Proceed to assign the county
-        newUserAdminPage.clickOnArrowRight();
-        Assert.assertTrue(newUserAdminPage.isCountyAssigned(countyMidlandMaps), "County has not been assigned.");
-
-        //click on Save button
-        newUserAdminPage.clickOnSaveButton();
-        //Success Message
-        Assert.assertTrue(newUserAdminPage.isSuccessUserUpdatedDisplayed(),"The user modification could not be modified.");
-        //Logout and log in using contract account
-        LoginPage newLoginPage = Logout(newExplorerPage.clickOnUserAdministrationPage());
-        MidlandMapsPage newMidlandMapsPage = ValidateContractAccess(environment,contractorUserName, contractorPassword, newLoginPage);
-
-        //Make sure that current contract user is able to see the previous assigned midland maps county
-        newMidlandMapsPage.clickOnCountyCombo(countyMidlandMaps);
-        Assert.assertTrue(newMidlandMapsPage.isMidlandMapCountyDisplayed(countyMidlandMaps),"Midland Map County has not been displayed." + countyMidlandMaps);
-
-    }
-
-    public LoginPage Logout(UserAdministrationPage newUserAdminPage){
-
-        //Validate if the user menu has been selected/displayed
-        Assert.assertTrue(newUserAdminPage.isMenuUserDisplayed(10), "Cannot display the User Menu");
-        //Click on User Administration item
-        LoginPage newLoginPage = newUserAdminPage.clickOnLogoutItem();
-        //Validate that User has been logged out
-        Assert.assertTrue(newLoginPage.isLoginStandardTitleDisplayed(), "Cannot access the Login Page.");
-        return newLoginPage;
-    }
-
-    public MidlandMapsPage ValidateContractAccess(String environment, String contractorUserName, String contractorPassword, LoginPage newLoginPage){
-
-        newLoginPage.clickOnStandaloneSignInButton(environment);
-        Assert.assertTrue(newLoginPage.isLoginStandaloneTitleDisplayed(), "Cannot access the StandAlone Page. Standalone Title not displayed");
-        newLoginPage.with(contractorUserName, contractorPassword);
-        newLoginPage.clickOnSignInButton();
-
-        ExplorerPage newExplorerPage = newLoginPage.clickOnHomeButton();
-        Assert.assertTrue(newExplorerPage.isExplorerMainPageDisplayed(), "Cannot access the Home Administration Page. (Search Results)");
+        ExplorerPage newExplorerPage = loginIntoCHMainPageTest.loginSuccessfullyTest(environment, contractorUserName, contractorPassword, getDriverInstance());
 
         MidlandMapsPage newMidlandMapsPage = newExplorerPage.clickOnMidlandMapsSearch();
-        return newMidlandMapsPage;
+
+        //Make sure that current contract user is able to see the previous assigned midland maps county
+        newMidlandMapsPage.clickOnCountyCombo(searchMidlandMapsCountyName);
+        Assert.assertTrue(newMidlandMapsPage.isMidlandMapCountyDisplayed(searchMidlandMapsCountyName),"Midland Map County has not been displayed." + searchMidlandMapsCountyName);
+
+        //Make sure that current contract user is able to see the previous assigned midland maps county
+        newMidlandMapsPage.clickOnYearCombo(searchMidlandMapsCountyYear);
+        Assert.assertTrue(newMidlandMapsPage.isMidlandMapCountyYearDisplayed(searchMidlandMapsCountyYear),"Midland Map County Year has not been displayed." +searchMidlandMapsCountyName + " " + searchMidlandMapsCountyYear);
+
+        //Click on Apply button
+        newMidlandMapsPage.clickOnApplyButton();
+        //Make sure that image (pdf file) has beed loaded/displayed successfully
+        //Wait until Progress Bar is gone
+        newMidlandMapsPage.isProgressBarDone();
+        //Wait until Search Results retrieves documents
+        Assert.assertFalse(newMidlandMapsPage.isDocumentNotAvailable(), "Document not available. (PDF Viewer: Document is empty)");
+
     }
+
 }
