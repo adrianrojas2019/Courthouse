0dcf3f577be47997aaca452db2d3111ddf7bfdcd
Adrian Rojas <rojasadrian@hotmail.com>
2019-07-24 13:53:18 -0600

    convert string to boolean

--- target/classes/main/java/selenium/SeleniumSetUp.java
+++ target/classes/main/java/selenium/SeleniumSetUp.java
@@ -49,9 +49,9 @@ public abstract class SeleniumSetUp {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName("chrome");
             capabilities.setVersion("75");
-            capabilities.setCapability("enableVNC", enableVNC);
-            capabilities.setCapability("enableLog", enableServerLog);
-            capabilities.setCapability("enableVideo", enableVideo);
+            capabilities.setCapability("enableVNC", Boolean.parseBoolean(enableVNC));
+            capabilities.setCapability("enableLog", Boolean.parseBoolean(enableServerLog));
+            capabilities.setCapability("enableVideo", Boolean.parseBoolean(enableVideo));
             //System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
             driver = new RemoteWebDriver(new URL("http://"+serverIPAddress+"/wd/hub"), capabilities);
         }
